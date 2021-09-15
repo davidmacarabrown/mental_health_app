58e2e9e0b069c540ef9d2bd3bc47ed8e6b4dc05d
davidmacarabrown <davidmacarabrown@gmail.com>
2021-09-15 11:12:02 +0100

    added spring initialised folder for back-end

---
+++ capstone-api/src/test/java/com/codeclan/example/capstoneapi/CapstoneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.capstoneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
