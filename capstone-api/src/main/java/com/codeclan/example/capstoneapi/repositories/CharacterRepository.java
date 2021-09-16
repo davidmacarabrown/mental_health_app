835c01d54e5313c88e997ec5f34a418cb60754e9
davidmacarabrown <davidmacarabrown@gmail.com>
2021-09-16 10:48:04 +0100

    initialised new folder to use SQL db

--- capstone-api/src/main/java/com/codeclan/example/capstoneapi/repositories/CharacterRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codeclan.example.capstoneapi.repositories;
-import com.codeclan.example.capstoneapi.models.character.Character;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CharacterRepository extends MongoRepository <Character, String> {
-}
